da282c2b7991c320e85836d8eadfa21b919e47c8
F0RT-DEV <douglasf935255@gmail.com>
2024-10-29 20:04:29 -0300

    renomendo eventos.js e script.js

--- participantes.js
+++ participantes.js
@@ -34,27 +34,3 @@ document.addEventListener("DOMContentLoaded", () => {
       form.reset();
   });
 });
-
-// Função para filtrar eventos com base na pesquisa
-function searchEvents() {
-    // Obter o valor da busca
-    const searchTerm = document.querySelector('.search-bar input').value.toLowerCase();
-    // Obter todos os eventos
-    const events = document.querySelectorAll('.event');
-    
-    // Iterar sobre os eventos e mostrar/ocultar com base na busca
-    events.forEach(event => {
-        const eventName = event.querySelector('h2').innerText.toLowerCase();
-        if (eventName.includes(searchTerm)) {
-            event.style.display = ''; // Mostrar evento
-        } else {
-            event.style.display = 'none'; // Ocultar evento
-        }
-    });
-}
-
-// Adicionar um evento ao botão de busca
-document.querySelector('.search-bar button').addEventListener('click', function(event) {
-    event.preventDefault(); // Evitar o envio do formulário
-    searchEvents(); // Chamar a função de busca
-});
